b6624a02bb640cf07af1d57e94a39826849f945a
zkaric1 <zkaric1@etf.unsa.ba>
2018-05-08 00:00:35 +0200

    ----

--- Projekat/TravelBookApp/TravelBookApp/App.xaml.cs
+++ Projekat/TravelBookApp/TravelBookApp/App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.WindowsAzure.MobileServices;
 
 namespace TravelBookApp
 {
@@ -26,7 +27,10 @@ namespace TravelBookApp
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
         /// </summary>
-        public App()
+        /// 
+       
+   public static MobileServiceClient MobileService = new MobileServiceClient("https://travelbookapp.azurewebsites.net");
+    public App()
         {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
