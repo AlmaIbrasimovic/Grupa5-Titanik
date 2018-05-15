0fe7bff93018424eedac1cf41bec5fc628e44f8f
zkaric1 <zkaric1@etf.unsa.ba>
2018-05-15 13:26:30 +0200

    -

--- Projekat/TravelBookApp/TravelBookApp/AzureKlase/ConnectionStringAzure.cs
+++ Projekat/TravelBookApp/TravelBookApp/AzureKlase/ConnectionStringAzure.cs
@@ -8,6 +8,6 @@ namespace TravelBookApp.AzureKlase
 {
     public class ConnectionStringAzure
     {
-        public String konekcija = "Server = tcp:travelbookserver.database.windows.net,1433;Initial Catalog = TravelBookDB; Persist Security Info=False;User ID ={ mujo }; Password={ Fata123. }; MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout = 30";
+        public String konekcija = "Server = tcp:travelbookserver.database.windows.net,1433;Initial Catalog = TravelBookDB; Persist Security Info=False;User ID = mujo ; Password= Fata123.; MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout = 30";
 }
 }
