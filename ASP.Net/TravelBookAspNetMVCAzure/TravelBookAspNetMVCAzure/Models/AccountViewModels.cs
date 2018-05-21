cf2f2eb67850d59d9fe22b3f989003b4b72fe9b4
Alma Ibrasimovic <aibrasimov1@etf.unsa.ba>
2018-05-21 21:30:17 +0200

    započet asp

--- ASP.Net/TravelBookAspNetMVCAzure/TravelBookAspNetMVCAzure/Models/AccountViewModels.cs
+++ ASP.Net/TravelBookAspNetMVCAzure/TravelBookAspNetMVCAzure/Models/AccountViewModels.cs
@@ -72,12 +72,12 @@ namespace TravelBookAspNetMVCAzure.Models
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Lozinka")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Potvrdi lozinku")]
+        [Compare("Password", ErrorMessage = "Lozinke se ne slažu.")]
         public string ConfirmPassword { get; set; }
     }
 
---
+++ ASP.Net/TravelBookAspNetMVCAzure/TravelBookAspNetMVCAzure/Models/TravelContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace TravelBookAspNetMVCAzure.Models
+{
+    public class TravelContext : DbContext
+    {
+        public TravelContext() : base("Data Source=tcp:travelbookserver.database.windows.net,1433;Initial Catalog=TravelBookDB;User ID=mujo;Password=Fata123.") { }
+
+
+        
+        //dodavanjem klasa iz modela kao DbSet iste će biti mapirane u bazu podataka
+       /* public DbSet<Student> Student { get; set; }
+        public DbSet<UpisNaPredmet> UpisNaPredmet { get; set; }
+        public DbSet<Predmet> Predmet { get; set; }*/
+        //Ova funkcija se koriste da bi se ukinulo automatsko dodavanje množine u nazive
+     /*   protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }*/
+    }
+}
