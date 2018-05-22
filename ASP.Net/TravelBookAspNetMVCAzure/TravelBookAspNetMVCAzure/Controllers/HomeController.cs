410d8250cbb1b76bc6b4dc1a39a9a18857a177a3
zkaric1 <zkaric1@etf.unsa.ba>
2018-05-23 01:38:38 +0200

    -

--- ASP.Net/TravelBookAspNetMVCAzure/TravelBookAspNetMVCAzure/Controllers/HomeController.cs
+++ ASP.Net/TravelBookAspNetMVCAzure/TravelBookAspNetMVCAzure/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace TravelBookAspNetMVCAzure.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "O agenciji";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "O agenciji";
 
             return View();
         }
