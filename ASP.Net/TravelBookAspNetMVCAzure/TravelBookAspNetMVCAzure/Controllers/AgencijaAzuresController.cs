119b59fbc4ed2dd4425facbc4946c3353b9980bd
Alma Ibrasimovic <aibrasimov1@etf.unsa.ba>
2018-05-24 03:10:38 +0200

    baza dodana KOJA RADI!!!!

---
+++ ASP.Net/TravelBookAspNetMVCAzure/TravelBookAspNetMVCAzure/Controllers/AgencijaAzuresController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TravelBookAspNetMVCAzure.Models;
+
+namespace TravelBookAspNetMVCAzure.Controllers
+{
+    public class AgencijaAzuresController : Controller
+    {
+        private TravelContext db = new TravelContext();
+
+        // GET: AgencijaAzures
+        public ActionResult Index()
+        {
+            return View(db.AgencijaAzures.ToList());
+        }
+
+        // GET: AgencijaAzures/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AgencijaAzure agencijaAzure = db.AgencijaAzures.Find(id);
+            if (agencijaAzure == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agencijaAzure);
+        }
+
+        // GET: AgencijaAzures/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AgencijaAzures/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,createdAt,updatedAt,version,deleted,naziv,idKartica,telefon,grad,lokacija,sifra,email")] AgencijaAzure agencijaAzure)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AgencijaAzures.Add(agencijaAzure);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(agencijaAzure);
+        }
+
+        // GET: AgencijaAzures/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AgencijaAzure agencijaAzure = db.AgencijaAzures.Find(id);
+            if (agencijaAzure == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agencijaAzure);
+        }
+
+        // POST: AgencijaAzures/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,createdAt,updatedAt,version,deleted,naziv,idKartica,telefon,grad,lokacija,sifra,email")] AgencijaAzure agencijaAzure)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(agencijaAzure).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(agencijaAzure);
+        }
+
+        // GET: AgencijaAzures/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AgencijaAzure agencijaAzure = db.AgencijaAzures.Find(id);
+            if (agencijaAzure == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agencijaAzure);
+        }
+
+        // POST: AgencijaAzures/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            AgencijaAzure agencijaAzure = db.AgencijaAzures.Find(id);
+            db.AgencijaAzures.Remove(agencijaAzure);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- ASP.Net/TravelBookAspNetMVCAzure/TravelBookAspNetMVCAzure/Models/TravelContext.cs
+++ ASP.Net/TravelBookAspNetMVCAzure/TravelBookAspNetMVCAzure/Models/TravelContext.cs
@@ -11,16 +11,23 @@ namespace TravelBookAspNetMVCAzure.Models
     {
         public TravelContext() : base("Data Source=tcp:travelbookserver.database.windows.net,1433;Initial Catalog=TravelBookDB;User ID=mujo;Password=Fata123.") { }
 
-
-        
-        //dodavanjem klasa iz modela kao DbSet iste će biti mapirane u bazu podataka
-       /* public DbSet<Student> Student { get; set; }
-        public DbSet<UpisNaPredmet> UpisNaPredmet { get; set; }
-        public DbSet<Predmet> Predmet { get; set; }*/
-        //Ova funkcija se koriste da bi se ukinulo automatsko dodavanje množine u nazive
-     /*   protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
-        }*/
+        }
+        public System.Data.Entity.DbSet<TravelBookAspNetMVCAzure.Models.KorisnikAzure> KorisnikAzures { get; set; }
+        public System.Data.Entity.DbSet<TravelBookAspNetMVCAzure.Models.HotelAzure> HotelAzures { get; set; }
+
+        public System.Data.Entity.DbSet<TravelBookAspNetMVCAzure.Models.AgencijaAzure> AgencijaAzures { get; set; }
+
+        public System.Data.Entity.DbSet<TravelBookAspNetMVCAzure.Models.DestinacijaAzure> DestinacijaAzures { get; set; }
+
+        public System.Data.Entity.DbSet<TravelBookAspNetMVCAzure.Models.PrevozAzure> PrevozAzures { get; set; }
+
+        public System.Data.Entity.DbSet<TravelBookAspNetMVCAzure.Models.PutovanjeAzure> PutovanjeAzures { get; set; }
+
+        public System.Data.Entity.DbSet<TravelBookAspNetMVCAzure.Models.RezervisanaPutovanjaAzure> RezervisanaPutovanjaAzures { get; set; }
+
+        public System.Data.Entity.DbSet<TravelBookAspNetMVCAzure.Models.KarticaAzure> KarticaAzures { get; set; }
     }
 }
