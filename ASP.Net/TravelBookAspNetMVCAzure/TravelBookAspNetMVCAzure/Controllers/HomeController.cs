0c1d79e626415dfb043945d4a5305bfcae903c85
Alma Ibrasimovic <aibrasimov1@etf.unsa.ba>
2018-06-01 15:20:44 +0200

    citanje iz baze popravljeno

--- ASP.Net/TravelBookAspNetMVCAzure/TravelBookAspNetMVCAzure/Controllers/HomeController.cs
+++ ASP.Net/TravelBookAspNetMVCAzure/TravelBookAspNetMVCAzure/Controllers/HomeController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelBookAspNetMVCAzure.Models;
 
 namespace TravelBookAspNetMVCAzure.Controllers
 {
+     
     public class HomeController : Controller
     {
+
+        private TravelContext db = new TravelContext();
         public ActionResult Index()
         {
-            return View();
+            var model = db.PutovanjeAzures.ToList();
+            return View(model);
         }
 
         public ActionResult About()
