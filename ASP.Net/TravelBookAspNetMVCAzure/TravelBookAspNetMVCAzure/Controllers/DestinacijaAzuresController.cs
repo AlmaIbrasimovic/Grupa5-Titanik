e62abd5a7240c8394aea5fae5cc581c7f1c8a19f
Alma Ibrasimovic <aibrasimov1@etf.unsa.ba>
2018-06-02 04:25:40 +0200

    čitanje slika iz baze dodano

--- ASP.Net/TravelBookAspNetMVCAzure/TravelBookAspNetMVCAzure/Controllers/DestinacijaAzuresController.cs
+++ ASP.Net/TravelBookAspNetMVCAzure/TravelBookAspNetMVCAzure/Controllers/DestinacijaAzuresController.cs
@@ -45,7 +45,18 @@ namespace TravelBookAspNetMVCAzure.Controllers
             }
             return ime;
         }
-   
+
+        public string vratiSliku(string id)
+        {
+            string slika = string.Empty;
+            DestinacijaAzure destinacijaAzure = db.DestinacijaAzures.Find(id);
+            if (destinacijaAzure != null)
+            {
+                slika = destinacijaAzure.slika;
+            }
+            return slika;
+        }
+
         // GET: DestinacijaAzures/Create
         public ActionResult Create()
         {
